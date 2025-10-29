11c0db03e5ac964b6d8dd05bb52b78511169524b
FRIZFOREVER <kirillhromov@mail.ru>
2025-10-30 01:38:01 +0300

    added message history pydantic class

--- ml/src/ml/agent/calls/ollama_calls.py
+++ ml/src/ml/agent/calls/ollama_calls.py
@@ -1,36 +1,34 @@
-import os
-
-from openai import OpenAI
-from ml.utils.formats import _rstrip_slash
-
-_OLLAMA_BASE_URL = "http://ollama:11434/v1/"
+# from ml.utils.formats import _rstrip_slash
+from ml.configs.model_config import ModelSettings
+from ollama import chat, embed
+from typing import Union, List, Dict
 
 
-def _get_env_var(key: str) -> str:
-    value = os.getenv(key)
-    if not value:
-        raise RuntimeError(f"Environment variable {key} must be set for Ollama calls.")
-    return value
+class _ReasoningModelClient:
+    def __init__(self, settings: ModelSettings):
+        self.s: ModelSettings = settings
+
+    def call(self, messages: List[Dict[str, str]]):
+        chat(model=self.s.model,
+             messages=messages)
+
+class _RerankModelClient:
+    def __init__(self, settings: ModelSettings):
+        self.s = settings
+
+class _EmbeddingModelClient:
+    def __init__(self, settings: ModelSettings):
+        self.s = settings
 
 
-class _OllamaClient:
-    def __init__(self, 
-                 *,
-                 env_key: str) -> None:
-        self.model_name = _get_env_var(env_key)
-        self.client = OpenAI(base_url=_OLLAMA_BASE_URL, api_key="ollama")
+_CLIENTS = {
+    "chat": _ReasoningModelClient,
+    "embeddings": _EmbeddingModelClient,
+    "reranker": _RerankModelClient,
+}
 
 
-class reasoning_model_client(_OllamaClient):
-    def __init__(self):
-        super().__init__("OLLAMA_REASONING_MODEL")
-
-
-class rerank_model_client(_OllamaClient):
-    def __init__(self):
-        super().__init__("OLLAMA_RERANK_MODEL")
-
-
-class embedding_model_client(_OllamaClient):
-    def __init__(self):
-        super().__init__("OLLAMA_EMBEDDING_MODEL")
+def make_client(settings: ModelSettings) -> Union[_ReasoningModelClient, 
+                                                  _EmbeddingModelClient, 
+                                                  _RerankModelClient]:
+    return _CLIENTS[settings.api_mode](settings)
