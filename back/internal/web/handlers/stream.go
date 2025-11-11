7c6d3f41210e4e036b78d348cc15db1662f4329d
Ubludor <90902613+Ubludor@users.noreply.github.com>
2025-11-11 18:39:14 +0300

    делаю

--- back/internal/web/handlers/stream.go
+++ back/internal/web/handlers/stream.go
@@ -2,8 +2,14 @@ package handlers
 
 import (
 	"database/sql"
+	"encoding/json"
 	"jabki/internal/client"
+	"jabki/internal/database"
+	"strconv"
+	"time"
 
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
 	"github.com/sirupsen/logrus"
 )
 
@@ -13,4 +19,45 @@ type Stream struct {
 	logger *logrus.Logger
 }
 
-type streamIn struct{}
+type streamIn struct{
+	Question string `json:"question"`
+	VoiceURL string `json:"voice_url"`
+}
+
+func (sh *Stream) Handler(c *fiber.Ctx) error {
+	questionTime := time.Now().UTC()
+	var messageIn messageIn
+	var err error
+
+	uuid := c.Locals("uuid").(uuid.UUID)
+	chatIDStr := c.Params("chat_id")
+
+	chatID, err := strconv.Atoi(chatIDStr)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Invalid chat ID format",
+		})
+	}
+
+	isCorresponds, err := database.CheckChat(sh.db, uuid.String(), chatID, sh.logger)
+	if err != nil || !isCorresponds {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Chat ID is not corresponds to user uuid",
+		})
+	}
+
+	if err = json.Unmarshal(c.Body(), &messageIn); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error":   "Invalid JSON format",
+			"details": err.Error(),
+		})
+	}
+
+	messages, err := database.GetHistory(sh.db, chatID, sh.logger)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error":   "Error in database",
+			"details": err.Error(),
+		})
+	}
+}
