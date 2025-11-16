d5cfc91d28c62daa29c0857edbab84518dcf55ed
Egor <egoryarovitsyn1@gmail.com>
2025-11-16 19:22:16 +0300

    add client graph_log

--- ml/src/ml/configs/message.py
+++ ml/src/ml/configs/message.py
@@ -78,6 +78,7 @@ class RequestPayload(BaseModel):
     system: str
     file_url: str
     is_voice: bool
+    answer_id: Optional[int] = None
 
     @field_validator("messages", mode="before")
     @classmethod
