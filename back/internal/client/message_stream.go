0e3ec27d6c42ea662c7c5dca292df4fb18ac5c1c
Ubludor <90902613+Ubludor@users.noreply.github.com>
2025-11-16 23:39:20 +0300

    заменяю null на пустые строки в client.Profile

--- back/internal/client/message_stream.go
+++ back/internal/client/message_stream.go
@@ -23,9 +23,9 @@ type Profile struct {
 	ID                     int     `json:"id"`
 	Login                  string  `json:"login"`
 	FIO                    string  `json:"username"`
-	UserInfo               *string `json:"user_info"`
-	BusinessInfo           *string `json:"business_info"`
-	AdditionalInstructions *string `json:"additional_instructions"`
+	UserInfo               string `json:"user_info"`
+	BusinessInfo           string `json:"business_info"`
+	AdditionalInstructions string `json:"additional_instructions"`
 }
 
 // PayloadStream структура для входных данных.
