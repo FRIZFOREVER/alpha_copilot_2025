edd88154a3e0fe9227d00c83016c03429478be69
Ubludor <90902613+Ubludor@users.noreply.github.com>
2025-12-01 16:51:59 +0300

    Добавил значение для выситывания процента

--- back/internal/database/queries/broad_analytic/count_tags.sql
+++ back/internal/database/queries/broad_analytic/count_tags.sql
@@ -1,6 +1,7 @@
 SELECT 
     q.tag,
-    COUNT(q.tag) as tag_count
+    COUNT(q.tag) as tag_count,
+    COUNT(CASE WHEN DATE(q.time_utc) != CURRENT_DATE THEN q.tag END) as tag_count_yesterday
 FROM questions q
 JOIN chats c ON q.chat_id = c.id
 JOIN users u ON c.user_id = u.id
