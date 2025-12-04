46b2aecbf912e55f7edb5c9700bd87048a72bd24
Egor <egoryarovitsyn1@gmail.com>
2025-12-04 23:55:10 +0300

    fix

--- frontend/src/entities/chat/api/chatService.tsx
+++ frontend/src/entities/chat/api/chatService.tsx
@@ -19,7 +19,7 @@ import {
   GetGraphLogsResponse,
 } from "../types/types";
 
-const API_BASE_URL = "http://127.0.0.1:8080";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 class ChatService {
   public async createChat(
--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -3,6 +3,15 @@
 /// <reference types="@testing-library/jest-dom" />
 /// <reference types="vite-plugin-svgr/client" />
 
+interface ImportMetaEnv {
+  readonly VITE_API_BASE_URL: string;
+  readonly VITE_ML_SERVICE_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
 declare module "*.svg" {
   import * as React from "react";
   const ReactComponent: React.FunctionComponent<
