8dcc779c81fe42b1fefe50c3e01d93a608ff1986
Хромов Кирилл Филиппович <kirillhromov@mail.ru>
2025-12-05 20:24:48 +0200

    Allow GraphState to hold async iterator

--- ml/src/ml/domain/workflow/agent/nodes/final_node/node.py
+++ ml/src/ml/domain/workflow/agent/nodes/final_node/node.py
@@ -16,7 +16,7 @@ async def final_stream(state: GraphState) -> GraphState:
         logger.error(msg)
         raise RuntimeError(msg)
 
-    result = client.stream(messages=prompt)
+    result = await client.stream(messages=prompt)
 
     state.output_stream = result
 
