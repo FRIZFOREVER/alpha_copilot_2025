2ac65ceb77ab230be13288384f5de0164ba575d6
Хромов Кирилл Филиппович <kirillhromov@mail.ru>
2025-12-06 10:40:11 +0200

    Extract OpenRouter provider helper to utils

--- ml/src/ml/utils/__init__.py
+++ ml/src/ml/utils/__init__.py
@@ -1,4 +1,5 @@
 from .download_formatters import format_bytes, format_progress
+from .openrouter import OPENROUTER_PROVIDER_BODY, apply_openrouter_provider
 from .pipeline_data_formatters import (
     format_research_observations,
     get_system_prompt,
@@ -9,4 +10,6 @@ __all__ = [
     "format_progress",
     "get_system_prompt",
     "format_research_observations",
+    "OPENROUTER_PROVIDER_BODY",
+    "apply_openrouter_provider",
 ]
