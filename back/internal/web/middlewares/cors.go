1a32160143c18b0c0990724098d4e1fe31984d8d
Ubludor <denis.r.maslov@gmail.com>
2025-12-06 19:44:22 +0300

    allowOrigins

--- back/internal/web/middlewares/cors.go
+++ back/internal/web/middlewares/cors.go
@@ -7,7 +7,7 @@ import (
 
 func Cors(allowOrigins string) fiber.Handler {
 	return cors.New(cors.Config{
-		AllowOrigins:     "*",                                           // Разрешенные источники
+		AllowOrigins:     allowOrigins,                                  // Разрешенные источники
 		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",                 // Разрешенные методы (добавлен OPTIONS для preflight)
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization", // Разрешенные заголовки
 		AllowCredentials: true,
