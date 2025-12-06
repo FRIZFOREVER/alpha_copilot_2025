5deabd3537e1dbc59eba86274814912aba4a6a3f
Хромов Кирилл Филиппович <kirillhromov@mail.ru>
2025-12-06 08:42:46 +0200

    Add LLM mode selector and provider-aware clients

--- ml/src/ml/configs/__init__.py
+++ ml/src/ml/configs/__init__.py
@@ -3,9 +3,14 @@ from ml.configs.ollama_client_settings import (
     EmbeddingClientSettings,
     ReasoningClientSettings,
 )
+from ml.configs.llm_mode import LLMMode, get_llm_mode, get_provider_api_key, get_provider_base_url
 
 __all__ = [
     "ReasoningClientSettings",
     "MODEL_ENV_VARS",
     "EmbeddingClientSettings",
+    "LLMMode",
+    "get_llm_mode",
+    "get_provider_base_url",
+    "get_provider_api_key",
 ]
